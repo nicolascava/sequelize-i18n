700e85eaca90c46d2b76b9f307f007aa37379197
nicolascava <me@nicolascava.com>
2020-05-08 19:23:50 +0200

    Refactoring tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,8 @@
 import Sequelize from "sequelize";
-import chai from "chai";
 import fs from "fs";
 
 import SequelizeI18N from ".";
 import logger from "./logger";
-
-chai.should();
-chai.use(require("chai-like"));
-chai.use(require("chai-things"));
 
 const languages = {
   list: ["FR", "EN", "ES"],
@@ -129,73 +124,76 @@ describe("SequelizeI18N", () => {
   });
 
   test("`getI18NName()` should return the i18n table1 name", () => {
-    i18n.getI18NName("random").should.equal("random_i18n");
+    expect(i18n.getI18NName("random")).toEqual("random_i18n");
   });
 
   test("`toArray()` of `null` should return an empty array", () => {
     const result = SequelizeI18N.toArray(null);
 
-    Array.isArray(result).should.equal(true);
-    result.length.should.equal(0);
+    expect(Array.isArray(result)).toEqual(true);
+    expect(result.length).toEqual(0);
   });
 
   test("`toArray()` of an empty array should return an empty array", () => {
     const result = SequelizeI18N.toArray([]);
 
-    Array.isArray(result).should.equal(true);
-    result.length.should.equal(0);
+    expect(Array.isArray(result)).toEqual(true);
+    expect(result.length).toEqual(0);
   });
 
   test("`toArray()` of an object should return an array that contains the object at index 0", () => {
     const obj = 5;
     const result = SequelizeI18N.toArray(obj);
 
-    Array.isArray(result).should.equal(true);
-    result.length.should.equal(1);
-    result[0].should.equal(obj);
+    expect(Array.isArray(result)).toEqual(true);
+    expect(result.length).toEqual(1);
+    expect(result[0]).toEqual(obj);
   });
 
   test("`getLanguageArrayType()` of an array of strings should return `STRING`", () => {
     const result = i18n.getLanguageArrayType();
 
-    result.should.equal("STRING");
+    expect(result).toEqual("STRING");
   });
 
   test("should have imported the example table1", () => {
-    sequelize.models.should.have.property("table1");
+    expect(sequelize.models).toHaveProperty("table1");
   });
 
   test("i18n should have the correct language list", () => {
-    i18n.options.languages.length.should.equal(languages.list.length);
+    expect(i18n.options.languages.length).toEqual(languages.list.length);
 
     for (let index = 0; index < languages.list.length; index += 1) {
-      i18n.options.languages[index].should.equal(languages.list[index]);
+      expect(i18n.options.languages[index]).toEqual(languages.list[index]);
     }
   });
 
   test(`i18n should have \`${languages.default}\` as default language`, () => {
-    i18n.options.defaultLanguage.should.equal(languages.default);
+    expect(i18n.options.defaultLanguage).toEqual(languages.default);
   });
 
   test("should have created the i18n table1 table", () => {
-    sequelize.models.should.have.property("table1_i18n");
+    expect(sequelize.models).toHaveProperty("table1_i18n");
   });
 
   test("should have a `table1`, `table1_i18ns` and `table2` tables", (done) => {
     sequelize
       .showAllSchemas()
       .then((result) => {
-        result.should.not.equal(null);
-        result.length.should.equal(3);
-        result.should.be
-          .an("array")
-          .that.contains.something.like({ name: "table1" });
-        result.should.be
-          .an("array")
-          .that.contains.something.like({ name: "table1_i18ns" });
-        result.should.be
-          .an("array")
-          .that.contains.something.like({ name: "table2" });
+        expect(result).not.toBeNull();
+        expect(result.length).toEqual(3);
+        expect(result).toBeInstanceOf(Array);
+        expect(result).toEqual(
+          expect.arrayContaining([expect.objectContaining({ name: "table1" })])
+        );
+        expect(result).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ name: "table1_i18ns" }),
+          ])
+        );
+        expect(result).toEqual(
+          expect.arrayContaining([expect.objectContaining({ name: "table2" })])
+        );
         done();
       })
       .catch((error) => done(error));
@@ -205,33 +203,41 @@ describe("SequelizeI18N", () => {
     table1
       .findByPk(1)
       .then((result) => {
-        result.should.have.property("table1_i18n");
-        result.table1_i18n.length.should.equal(2);
-        result.table1_i18n.should.be.an("array").that.contains.something.like({
-          label: "test",
-          description: "c'est un test",
-        });
-        result.table1_i18n.should.be.an("array").that.contains.something.like({
-          label: "test EN",
-          description: "This is a test",
-        });
+        expect(result).toHaveProperty("table1_i18n");
+        expect(result.table1_i18n.length).toEqual(2);
+        expect(result.table1_i18n).toBeInstanceOf(Array);
+        expect(result.table1_i18n).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              label: "test",
+              description: "c'est un test",
+            }),
+          ])
+        );
+        expect(result.table1_i18n).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              label: "test EN",
+              description: "This is a test",
+            }),
+          ])
+        );
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should return i18n values when the filter is on the i18n field", (done) => {
     table1
       .findOne({ where: { label: "test" } })
       .then((result) => {
-        result.should.have.property("table1_i18n");
-        result.table1_i18n.length.should.equal(1);
-        result.table1_i18n[0].should.have.property("label");
-        result.table1_i18n[0].label.should.equal("test");
-
+        expect(result).toHaveProperty("table1_i18n");
+        expect(result.table1_i18n.length).toEqual(1);
+        expect(result.table1_i18n[0]).toHaveProperty("label");
+        expect(result.table1_i18n[0].label).toEqual("test");
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should return English i18n values when the filter has include", (done) => {
@@ -246,13 +252,12 @@ describe("SequelizeI18N", () => {
         ],
       })
       .then((result) => {
-        result.should.have.property("table1_i18n");
-        result.table1_i18n[0].should.have.property("description");
-        result.table1_i18n[0].description.should.equal("This is a test");
-
+        expect(result).toHaveProperty("table1_i18n");
+        expect(result.table1_i18n[0]).toHaveProperty("description");
+        expect(result.table1_i18n[0].description).toEqual("This is a test");
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should return English i18n values using the function", (done) => {
@@ -261,11 +266,11 @@ describe("SequelizeI18N", () => {
       .then((result) => {
         const i18nResult = result.getI18N("EN");
 
-        i18nResult.should.have.property("description");
-        i18nResult.description.should.equal("This is a test");
+        expect(i18nResult).toHaveProperty("description");
+        expect(i18nResult.description).toEqual("This is a test");
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should return updated English i18n values", (done) => {
@@ -277,24 +282,24 @@ describe("SequelizeI18N", () => {
       .then((upd) => {
         const i18nUpdate = upd.getI18N("EN");
 
-        i18nUpdate.should.have.property("label");
-        i18nUpdate.should.have.property("description");
-        i18nUpdate.description.should.equal("This is a test");
-        i18nUpdate.label.should.equal("Test EN renamed");
+        expect(i18nUpdate).toHaveProperty("label");
+        expect(i18nUpdate).toHaveProperty("description");
+        expect(i18nUpdate.description).toEqual("This is a test");
+        expect(i18nUpdate.label).toEqual("Test EN renamed");
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should return the hard-coded language ID", (done) => {
     table1
       .findByPk(1, { language_id: "EN" })
       .then((r) => {
-        r.should.have.property("language_id");
-        r.language_id.should.equal("EN");
+        expect(r).toHaveProperty("language_id");
+        expect(r.language_id).toEqual("EN");
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should delete current instance and its i18n values", () => {
@@ -315,7 +320,7 @@ describe("SequelizeI18N with a different suffix", () => {
     sequelize = new Sequelize("", "", "", {
       dialect: "sqlite",
       storage: dbFile,
-      logging: false, // logger.log,
+      logging: false,
     });
 
     await sequelize.authenticate();
@@ -348,26 +353,28 @@ describe("SequelizeI18N with a different suffix", () => {
   });
 
   test("`getI18NName()` should return the i18n name for that table", () => {
-    i18n.getI18NName("random").should.equal("random-international");
+    expect(i18n.getI18NName("random")).toEqual("random-international");
   });
 
   test("should have created the international table1 table", () => {
-    sequelize.models.should.have.property("table1-international");
+    expect(sequelize.models).toHaveProperty("table1-international");
   });
 
   test("should have a `table1` and `table1-international` tables", (done) => {
     sequelize
       .showAllSchemas()
       .then((result) => {
-        result.should.not.equal(null);
-        result.length.should.equal(3);
-        result.should.be
-          .an("array")
-          .that.contains.something.like({ name: "table1" });
-        result.should.be
-          .an("array")
-          .that.contains.something.like({ name: "table1-internationals" });
-
+        expect(result).not.toBeNull();
+        expect(result.length).toEqual(3);
+        expect(result).toBeInstanceOf(Array);
+        expect(result).toEqual(
+          expect.arrayContaining([expect.objectContaining({ name: "table1" })])
+        );
+        expect(result).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ name: "table1-internationals" }),
+          ])
+        );
         done();
       })
       .catch((error) => done(error));
@@ -377,36 +384,41 @@ describe("SequelizeI18N with a different suffix", () => {
     table1
       .findByPk(1)
       .then((result) => {
-        result.should.have.property("table1-international");
-        result["table1-international"].length.should.equal(2);
-        result["table1-international"].should.be
-          .an("array")
-          .that.contains.something.like({
-            label: "test",
-            description: "c'est un test",
-          });
-        result["table1-international"].should.be
-          .an("array")
-          .that.contains.something.like({
-            label: "test EN",
-            description: "This is a test",
-          });
+        expect(result).toHaveProperty("table1-international");
+        expect(result["table1-international"].length).toEqual(2);
+        expect(result["table1-international"]).toBeInstanceOf(Array);
+        expect(result["table1-international"]).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              label: "test",
+              description: "c'est un test",
+            }),
+          ])
+        );
+        expect(result["table1-international"]).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              label: "test EN",
+              description: "This is a test",
+            }),
+          ])
+        );
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   test("should return i18n values when the filter is on the i18n field", (done) => {
     table1
       .findOne({ where: { label: "test" } })
       .then((result) => {
-        result.should.have.property("table1-international");
-        result["table1-international"].length.should.equal(1);
-        result["table1-international"][0].should.have.property("label");
-        result["table1-international"][0].label.should.equal("test");
+        expect(result).toHaveProperty("table1-international");
+        expect(result["table1-international"].length).toEqual(1);
+        expect(result["table1-international"][0]).toHaveProperty("label");
+        expect(result["table1-international"][0].label).toEqual("test");
         done();
       })
-      .catch((e) => e);
+      .catch((error) => done(error));
   });
 
   // TODO: add tests to check if model update works.
