4f1cde799650d07129c80085be9382f92439ee7e
nicolascava <me@nicolascava.com>
2020-06-27 12:19:49 +0200

    Updating dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,19 @@
 module.exports = {
   root: true,
-  extends: [
-    "airbnb",
-    "plugin:prettier/recommended"
-  ],
+  extends: ["airbnb", "plugin:prettier/recommended"],
   parser: "babel-eslint",
   env: {
     node: true,
     es6: true,
     jest: true,
-    mocha: true
+    mocha: true,
   },
-  plugins: [
-    "babel",
-    "prettier",
-    "chai-friendly",
-    "import"
-  ],
+  plugins: ["babel", "prettier", "chai-friendly", "import"],
   settings: {
     "import/resolver": {
       node: {
-        extensions: [".js"]
-      }
-    }
-  }
+        extensions: [".js"],
+      },
+    },
+  },
 };
